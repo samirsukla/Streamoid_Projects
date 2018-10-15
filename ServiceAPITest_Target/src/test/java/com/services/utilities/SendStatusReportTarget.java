3dc29107ffe784a7c74130fcfff5c6ea4cfdd228
Samir <samir@streamoid.com>
2018-10-15 22:58:45 +0530

    Added Mail Restriction Rule

--- ServiceAPITest_Target/src/test/java/com/services/utilities/SendStatusReportTarget.java
+++ ServiceAPITest_Target/src/test/java/com/services/utilities/SendStatusReportTarget.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.testng.annotations.Test;
 
 import com.amazonaws.regions.Regions;
@@ -20,20 +24,26 @@ import com.services.utilities.GetSystemDate;
 
 public class SendStatusReportTarget {
 	static String statusReportLocation = "target/surefire-reports/emailable-report.html";
-
+	static String[] sendCC;
 	static GetSystemDate getDate;
 	static final String FROM = "test-automation@streamoid.com";
-
-	String[] sendTo = new String[] { "kinshuk@streamoid.com", "hemang@streamoid.com" };
-	String[] sendCC = new String[] { "sar@streamoid.com", "prathaban@streamoid.com", "dash@streamoid.com",
-			"naveen@streamoid.com", "samir@streamoid.com" };
+	String[] sendTo = new String[] {"samir@streamoid.com", "naveen@streamoid.com", "murtaza.ali@streamoid.com"};
+	String[] sendCCPass = new String[] { "kinshuk@streamoid.com", "hemang@streamoid.com" };
+	String[] sendCCFail = new String[] { "sar@streamoid.com", "prathaban@streamoid.com", "dash@streamoid.com","kinshuk@streamoid.com", "hemang@streamoid.com"};
 
 	@Test
 	public void sendMail() throws IOException {
 
 		getDate = new GetSystemDate();
 		String todaysDate = getDate.getPresentDate();
-
+		boolean testStatus = sendMailStatus();
+		if( testStatus == true){
+			sendCC =sendCCPass;
+		}
+		else {
+			sendCC = sendCCFail;
+		
+		}
 		StringWriter writer = new StringWriter();
 		IOUtils.copy(new FileInputStream(new File(statusReportLocation)), writer, "ISO-8859-1");
 		String HTMLBODY = writer.toString();
@@ -57,6 +67,30 @@ public class SendStatusReportTarget {
 			System.out.println("The email was not sent. Error message: " + ex.getMessage());
 		}
 
+	}
+	public boolean sendMailStatus() throws IOException {
+		File file = new File(statusReportLocation);
+		Document doc = Jsoup.parse(file, "UTF-8");
+		Element body = doc.body();
+		Element table = body.select("table").get(0);
+		Elements rows = table.select("tr");
+		Element row = rows.get(2);
+		Elements cols = row.select("td");
+		int skippedValue = Integer.parseInt(cols.get(2).text());
+		//System.out.println("No of Tests Skipped : "+skippedValue);
+		int failedValue = Integer.parseInt(cols.get(3).text());
+		//System.out.println("No of Tests Failed : "+failedValue);
+		
+		if(skippedValue == 0 && failedValue == 0) {
+			return true;
+			
+		}
+		else {
+			return false;
+		}
+		
+		
+		
 	}
 
 }
